49fd73d58033a5fc9d58a1a0e9dc586c5eb0694a
Mohammed Hassan <mohammed.hassan@basf.com>
2021-12-15 17:05:48 +0100

    more code

--- include.cpp
+++ include.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include "weekmoLib.hpp"
+using namespace std;
+
+template <typename T>
+T maxof(const T &a, const T &b){
+    return (a > b ? a : b);
+}
 
 int main(){
     
+    cout << "Max is: " << maxof<int>(30, 20) << endl;
+
     printf("Function from weekmoLib %s\n", u8_to_bstr(50));
     return 0;
 }
