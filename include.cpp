efd49f39cf43397a452eb26484404fa01ff8b8a5
Mohammed Hassan <mohammed.hassan@basf.com>
2021-12-15 13:15:13 +0100

    more code

---
+++ include.cpp
@@ -0,0 +1,7 @@
+#include "weekmoLib.hpp"
+
+int main(){
+    
+    printf("Function from weekmoLib %s\n", u8_to_bstr(50));
+    return 0;
+}
--- weekmoLib.hpp
+++ weekmoLib.hpp
@@ -1,3 +1,9 @@
-#include <cstdint> 
+#ifndef WEEKMOLIB_H_
+#define WEEKMOLIB_H_
+
+#include <cstdint>
 #include <cstdio>
-const char * u8_to_bstr(uint8_t &);
+
+const char *u8_to_bstr(const uint8_t &);
+
+#endif /* WEEKMOLIB_H_ */
